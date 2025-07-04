0814b0be31b267acbbde7a7172366350ac4935f1
FELIX ANTONIO MERCEDES MERCEDES <Proyecto_Parcial>
2025-07-04 08:01:34 -0400

    he agregado un clase para las explociones de los ya que este es un sprite shett he cargado el sprite por frames y le coloque un (def update) para actualizarlo, agregue una clase para los powerups del juego y como estas son imagenes le he dado su utilidad correspondiente por imagen (Ejempli, si es una estrella el jugador puede ser vulnerable durante 5 seg) coloque un indicador de daño estilo free fire para los enemigos y el jugador para evitar posibles bug he colocado un (update) para ir actualizando el indicador de daño y evitar futuros bug del mismo

--- scripts/personajes.py
+++ scripts/personajes.py
@@ -278,3 +278,373 @@ class Jugador(pygame.sprite.Sprite):
 
         self.rect.x += dx
         self.rect.y += dy
+
+        #coliciones contra obstaculos
+        self.colision_obstaculos(dx, 0, grupo_obstaculos)
+        self.colision_obstaculos(0, dy, grupo_obstaculos)
+
+        #coliciones entre jugador y enemigos
+        for enemy in grupo_enemigos_param:
+            if enemy != self and self.rect.colliderect(enemy.rect):
+                if self.rect.centerx < enemy.rect.centerx:
+                    self.rect.x -= 1
+                else:
+                    self.rect.x += 1
+                if self.rect.centery < enemy.rect.centery:
+                    self.rect.y -= 1
+                else:
+                    self.rect.y += 1
+
+        self.rect.left = max(0, self.rect.left)
+        self.rect.right = min(WORLD_WIDTH, self.rect.right)
+        self.rect.top = max(0, self.rect.top)
+        self.rect.bottom = min(WORLD_HEIGHT, self.rect.bottom)
+
+        if self.es_invulnerable:
+            if ahora - self.ultima_colision_tiempo > PLAYER_INVULNERABILITY_DURATION:
+                self.es_invulnerable = False
+        
+        if self.invulnerable_powerup_activo:
+            if ahora - self.tiempo_inicio_invulnerabilidad_powerup > self.duracion_invulnerabilidad_powerup:
+                self.invulnerable_powerup_activo = False
+
+        if self.bonus_x2_activo:
+            if ahora - self.tiempo_inicio_bonus_x2 > self.duracion_bonus_x2:
+                self.bonus_x2_activo = False
+
+        if moving:
+            if ahora - self.ultima_actualizacion_frame > self.velocidad_animacion:
+                self.ultima_actualizacion_frame = ahora
+                if self.direction in self.animation_frames and self.animation_frames[self.direction]:
+                    self.current_frame = (self.current_frame + 1) % len(self.animation_frames[self.direction])
+                    self.image = self.animation_frames[self.direction][self.current_frame]
+        else:
+            self.current_frame = 0
+            if self.direction in self.animation_frames and self.animation_frames[self.direction]:
+                self.image = self.animation_frames[self.direction][self.current_frame]
+
+        #calculamos la distancia del enemigo mas cercano para usar una pequeña asistencia de aim
+        self.target_enemy_pos = None
+        closest_enemy_dist_sq = AIM_ASSIST_RANGE ** 2
+
+        all_enemies = list(grupo_enemigos) + list(grupo_torretas)
+        for enemy in all_enemies:
+            dist_x = enemy.rect.centerx - self.rect.centerx
+            dist_y = enemy.rect.centery - self.rect.centery
+            dist_sq = dist_x ** 2 + dist_y ** 2
+
+            if dist_sq < closest_enemy_dist_sq:
+                closest_enemy_dist_sq = dist_sq
+                self.target_enemy_pos = enemy.rect.center
+
+    def recibir_daño(self, daño):
+        """Método para que el jugador reciba daño"""
+        if self.invulnerable_powerup_activo:
+            return
+        if self.es_invulnerable:
+            return
+
+        self.vida_actual_corazon -= daño
+        if self.vida_actual_corazon <= 0:
+            self.corazones -= 1
+            if self.corazones > 0:
+                self.vida_actual_corazon = self.vida_por_corazon
+            else:
+                self.vida_actual_corazon = 0
+        
+        #creamos un indicador de daño para el jugador estilo free fire
+        grupo_indicadores_daño_jugador.add(IndicadorDanoJugador(self.rect.centerx, self.rect.top, daño))
+
+        self.es_invulnerable = True
+        self.ultima_colision_tiempo = pygame.time.get_ticks()
+
+    def colision_obstaculos(self, dx, dy, grupo_obstaculos):
+        """Manejo de colisiones con obstáculos"""
+        for obstaculo in grupo_obstaculos:
+            if obstaculo.tipo in ["solido", "destructible"] and self.rect.colliderect(obstaculo.rect):
+                if dx > 0:
+                    self.rect.right = obstaculo.rect.left
+                if dx < 0:
+                    self.rect.left = obstaculo.rect.right
+                if dy > 0:
+                    self.rect.bottom = obstaculo.rect.top
+                if dy < 0:
+                    self.rect.top = obstaculo.rect.bottom
+            elif obstaculo.tipo == "peligro" and self.rect.colliderect(obstaculo.rect):
+                self.recibir_daño(OBSTACLE_DAMAGE)
+
+    def disparar(self, grupo_enemigos_param):
+        """Método para que el jugador dispare"""
+        ahora = pygame.time.get_ticks()
+        if ahora - self.ultimo_disparo > self.tiempo_entre_disparos:
+            self.ultimo_disparo = ahora
+
+            self.target_enemy_pos = None
+            closest_enemy_dist_sq = AIM_ASSIST_RANGE ** 2
+
+            all_enemies = list(grupo_enemigos_param) + list(grupo_torretas)
+            for enemy in all_enemies:
+                dist_x = enemy.rect.centerx - self.rect.centerx
+                dist_y = enemy.rect.centery - self.rect.centery
+                dist_sq = dist_x ** 2 + dist_y ** 2
+
+                if dist_sq < closest_enemy_dist_sq:
+                    closest_enemy_dist_sq = dist_sq
+                    self.target_enemy_pos = enemy.rect.center
+
+            direccion_final_disparo = self.direccion_disparo 
+
+            if self.target_enemy_pos:
+                target_dx = self.target_enemy_pos[0] - self.rect.centerx
+                target_dy = self.target_enemy_pos[1] - self.rect.centery
+                mag = math.sqrt(target_dx ** 2 + target_dy ** 2)
+                if mag > 0:
+                    direccion_final_disparo = (target_dx / mag, target_dy / mag)
+
+            damage_to_deal = random.randint(BULLET_DAMAGE_MIN, BULLET_DAMAGE_MAX)
+            damage_to_deal = damage_to_deal * 2 if self.bonus_x2_activo else damage_to_deal
+            return Bala(self.rect.centerx, self.rect.centery, direccion_final_disparo, es_jugador=True, daño=damage_to_deal)
+        return None
+
+    def draw_aim_assist(self, screen):
+        pass
+
+    def activar_invulnerabilidad(self):
+        """Activar el power-up de invulnerabilidad"""
+        self.invulnerable_powerup_activo = True
+        self.tiempo_inicio_invulnerabilidad_powerup = pygame.time.get_ticks()
+
+    def activar_bonus_x2(self):
+        """Activar el power-up de daño doble"""
+        self.bonus_x2_activo = True
+        self.tiempo_inicio_bonus_x2 = pygame.time.get_ticks()
+
+    def aumentar_corazon(self):
+        """Aumentar un corazón de vida"""
+        if self.corazones < self.max_corazones:
+            self.corazones += 1
+            self.vida_actual_corazon = self.vida_por_corazon
+
+class Bala(pygame.sprite.Sprite):
+    """Clase para las balas del juego"""
+    def __init__(self, x, y, direccion, es_jugador=True, daño=None):
+        super().__init__()
+        self.original_image = pygame.Surface((10, 5), pygame.SRCALPHA)
+        self.original_image.fill(ROJO if es_jugador else AMARILLO)
+        self.image = self.original_image
+        self.rect = self.image.get_rect(center=(x, y))
+
+        self.speed = BULLET_SPEED if es_jugador else ENEMY_BULLET_SPEED
+        self.dx = direccion[0] * self.speed
+        self.dy = direccion[1] * self.speed
+
+        self.daño = daño if daño is not None else (random.randint(BULLET_DAMAGE_MIN, BULLET_DAMAGE_MAX) if es_jugador else ENEMY_BULLET_DAMAGE)
+        self.distancia_recorrida = 0
+        self.alcance_maximo = 400
+        self.es_jugador = es_jugador
+
+        #rotamos la pantalla segun la direccion del jugador
+        if self.dx == 0 and self.dy == 0:
+            angle = 0
+        else:
+            angle = math.degrees(math.atan2(-self.dy, self.dx))
+        self.image = pygame.transform.rotate(self.original_image, angle)
+        self.rect = self.image.get_rect(center=self.rect.center)
+
+    def update(self, offset_x, offset_y):
+        """Actualizar la posición de la bala"""
+        self.rect.x += self.dx
+        self.rect.y += self.dy
+        self.distancia_recorrida += math.sqrt(self.dx ** 2 + self.dy ** 2)
+
+        #eliminamos las balas del jugador que se salen de los limines establecidos
+        if (self.rect.left > WORLD_WIDTH or self.rect.right < 0 or
+                self.rect.top > WORLD_HEIGHT or self.rect.bottom < 0 or
+                self.distancia_recorrida > self.alcance_maximo):
+            self.kill()
+
+
+        #coliciones contra obstaculos
+        self.colision_obstaculos(dx, 0, grupo_obstaculos)
+        self.colision_obstaculos(0, dy, grupo_obstaculos)
+
+        #coliciones entre jugador y enemigos
+        for enemy in grupo_enemigos_param:
+            if enemy != self and self.rect.colliderect(enemy.rect):
+                if self.rect.centerx < enemy.rect.centerx:
+                    self.rect.x -= 1
+                else:
+                    self.rect.x += 1
+                if self.rect.centery < enemy.rect.centery:
+                    self.rect.y -= 1
+                else:
+                    self.rect.y += 1
+
+        self.rect.left = max(0, self.rect.left)
+        self.rect.right = min(WORLD_WIDTH, self.rect.right)
+        self.rect.top = max(0, self.rect.top)
+        self.rect.bottom = min(WORLD_HEIGHT, self.rect.bottom)
+
+        if self.es_invulnerable:
+            if ahora - self.ultima_colision_tiempo > PLAYER_INVULNERABILITY_DURATION:
+                self.es_invulnerable = False
+        
+        if self.invulnerable_powerup_activo:
+            if ahora - self.tiempo_inicio_invulnerabilidad_powerup > self.duracion_invulnerabilidad_powerup:
+                self.invulnerable_powerup_activo = False
+
+        if self.bonus_x2_activo:
+            if ahora - self.tiempo_inicio_bonus_x2 > self.duracion_bonus_x2:
+                self.bonus_x2_activo = False
+
+        if moving:
+            if ahora - self.ultima_actualizacion_frame > self.velocidad_animacion:
+                self.ultima_actualizacion_frame = ahora
+                if self.direction in self.animation_frames and self.animation_frames[self.direction]:
+                    self.current_frame = (self.current_frame + 1) % len(self.animation_frames[self.direction])
+                    self.image = self.animation_frames[self.direction][self.current_frame]
+        else:
+            self.current_frame = 0
+            if self.direction in self.animation_frames and self.animation_frames[self.direction]:
+                self.image = self.animation_frames[self.direction][self.current_frame]
+
+        #calculamos la distancia del enemigo mas cercano para usar una pequeña asistencia de aim
+        self.target_enemy_pos = None
+        closest_enemy_dist_sq = AIM_ASSIST_RANGE ** 2
+
+        all_enemies = list(grupo_enemigos) + list(grupo_torretas)
+        for enemy in all_enemies:
+            dist_x = enemy.rect.centerx - self.rect.centerx
+            dist_y = enemy.rect.centery - self.rect.centery
+            dist_sq = dist_x ** 2 + dist_y ** 2
+
+            if dist_sq < closest_enemy_dist_sq:
+                closest_enemy_dist_sq = dist_sq
+                self.target_enemy_pos = enemy.rect.center
+
+    def recibir_daño(self, daño):
+        """Método para que el jugador reciba daño"""
+        if self.invulnerable_powerup_activo:
+            return
+        if self.es_invulnerable:
+            return
+
+        self.vida_actual_corazon -= daño
+        if self.vida_actual_corazon <= 0:
+            self.corazones -= 1
+            if self.corazones > 0:
+                self.vida_actual_corazon = self.vida_por_corazon
+            else:
+                self.vida_actual_corazon = 0
+        
+        #creamos un indicador de daño para el jugador estilo free fire
+        grupo_indicadores_daño_jugador.add(IndicadorDanoJugador(self.rect.centerx, self.rect.top, daño))
+
+        self.es_invulnerable = True
+        self.ultima_colision_tiempo = pygame.time.get_ticks()
+
+    def colision_obstaculos(self, dx, dy, grupo_obstaculos):
+        """Manejo de colisiones con obstáculos"""
+        for obstaculo in grupo_obstaculos:
+            if obstaculo.tipo in ["solido", "destructible"] and self.rect.colliderect(obstaculo.rect):
+                if dx > 0:
+                    self.rect.right = obstaculo.rect.left
+                if dx < 0:
+                    self.rect.left = obstaculo.rect.right
+                if dy > 0:
+                    self.rect.bottom = obstaculo.rect.top
+                if dy < 0:
+                    self.rect.top = obstaculo.rect.bottom
+            elif obstaculo.tipo == "peligro" and self.rect.colliderect(obstaculo.rect):
+                self.recibir_daño(OBSTACLE_DAMAGE)
+
+    def disparar(self, grupo_enemigos_param):
+        """Método para que el jugador dispare"""
+        ahora = pygame.time.get_ticks()
+        if ahora - self.ultimo_disparo > self.tiempo_entre_disparos:
+            self.ultimo_disparo = ahora
+
+            self.target_enemy_pos = None
+            closest_enemy_dist_sq = AIM_ASSIST_RANGE ** 2
+
+            all_enemies = list(grupo_enemigos_param) + list(grupo_torretas)
+            for enemy in all_enemies:
+                dist_x = enemy.rect.centerx - self.rect.centerx
+                dist_y = enemy.rect.centery - self.rect.centery
+                dist_sq = dist_x ** 2 + dist_y ** 2
+
+                if dist_sq < closest_enemy_dist_sq:
+                    closest_enemy_dist_sq = dist_sq
+                    self.target_enemy_pos = enemy.rect.center
+
+            direccion_final_disparo = self.direccion_disparo 
+
+            if self.target_enemy_pos:
+                target_dx = self.target_enemy_pos[0] - self.rect.centerx
+                target_dy = self.target_enemy_pos[1] - self.rect.centery
+                mag = math.sqrt(target_dx ** 2 + target_dy ** 2)
+                if mag > 0:
+                    direccion_final_disparo = (target_dx / mag, target_dy / mag)
+
+            damage_to_deal = random.randint(BULLET_DAMAGE_MIN, BULLET_DAMAGE_MAX)
+            damage_to_deal = damage_to_deal * 2 if self.bonus_x2_activo else damage_to_deal
+            return Bala(self.rect.centerx, self.rect.centery, direccion_final_disparo, es_jugador=True, daño=damage_to_deal)
+        return None
+
+    def draw_aim_assist(self, screen):
+        pass
+
+    def activar_invulnerabilidad(self):
+        """Activar el power-up de invulnerabilidad"""
+        self.invulnerable_powerup_activo = True
+        self.tiempo_inicio_invulnerabilidad_powerup = pygame.time.get_ticks()
+
+    def activar_bonus_x2(self):
+        """Activar el power-up de daño doble"""
+        self.bonus_x2_activo = True
+        self.tiempo_inicio_bonus_x2 = pygame.time.get_ticks()
+
+    def aumentar_corazon(self):
+        """Aumentar un corazón de vida"""
+        if self.corazones < self.max_corazones:
+            self.corazones += 1
+            self.vida_actual_corazon = self.vida_por_corazon
+
+class Bala(pygame.sprite.Sprite):
+    """Clase para las balas del juego"""
+    def __init__(self, x, y, direccion, es_jugador=True, daño=None):
+        super().__init__()
+        self.original_image = pygame.Surface((10, 5), pygame.SRCALPHA)
+        self.original_image.fill(ROJO if es_jugador else AMARILLO)
+        self.image = self.original_image
+        self.rect = self.image.get_rect(center=(x, y))
+
+        self.speed = BULLET_SPEED if es_jugador else ENEMY_BULLET_SPEED
+        self.dx = direccion[0] * self.speed
+        self.dy = direccion[1] * self.speed
+
+        self.daño = daño if daño is not None else (random.randint(BULLET_DAMAGE_MIN, BULLET_DAMAGE_MAX) if es_jugador else ENEMY_BULLET_DAMAGE)
+        self.distancia_recorrida = 0
+        self.alcance_maximo = 400
+        self.es_jugador = es_jugador
+
+        #rotamos la pantalla segun la direccion del jugador
+        if self.dx == 0 and self.dy == 0:
+            angle = 0
+        else:
+            angle = math.degrees(math.atan2(-self.dy, self.dx))
+        self.image = pygame.transform.rotate(self.original_image, angle)
+        self.rect = self.image.get_rect(center=self.rect.center)
+
+    def update(self, offset_x, offset_y):
+        """Actualizar la posición de la bala"""
+        self.rect.x += self.dx
+        self.rect.y += self.dy
+        self.distancia_recorrida += math.sqrt(self.dx ** 2 + self.dy ** 2)
+
+        #eliminamos las balas del jugador que se salen de los limines establecidos
+        if (self.rect.left > WORLD_WIDTH or self.rect.right < 0 or
+                self.rect.top > WORLD_HEIGHT or self.rect.bottom < 0 or
+                self.distancia_recorrida > self.alcance_maximo):
+            self.kill()
+
