ed821e33467f31b554bd3457e29da1f1bd43763d
FELIX ANTONIO MERCEDES MERCEDES <Proyecto_Parcial>
2025-07-03 21:25:59 -0400

    Hemos agregado las variables del juego que tendremos

--- main.py
+++ main.py
@@ -8,3 +8,14 @@ from scripts.configuracion import *
 from scripts.personajes import *
 from scripts.enemigos import *
 from scripts.astar import a_star
+
+#aqui pondremos las variables para el juego
+estado_actual = ESTADO_INTRO
+juego_corriendo  = True
+jugador = None
+num_enemigos_por_zona = 5 #para el juego tendremos 5 enemigos por zona de camara en la pantalla
+#esto lo hacemos para poder tener un buen rendimiento en el juego
+num_torretas_por_zona = 1
+sonido_mapa_actual = None
+sonido_derrota_reproduciondose = False
+sonido_victoria_reproduciendose = False
