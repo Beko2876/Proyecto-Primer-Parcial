c773b280b583c2e9b98268ef291d693947450304
FELIX ANTONIO MERCEDES MERCEDES <Proyecto_Parcial>
2025-07-03 20:39:05 -0400

    asdasd

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+import math
+import random
+import os
+import sys
+import json
+from scripts.configuracion import *
+from scripts.personajes import *
+from scripts.enemigos import *
+from scripts.astar import a_star
